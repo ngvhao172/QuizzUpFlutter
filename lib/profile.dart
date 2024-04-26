15a2179d1478433ac3b0020d0ff4ddba589602fc
Phạm Nhật Quỳnh <52100100@student.tdtu.edu.vn>
2024-04-27 04:38:26 +0700

    profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -6,9 +7,58 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {},
+        ),
+        title: const Text("Profile"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/user.png'),
+              radius: 50,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Pham Nhat Quynh",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 65, 65, 65)),
+            ),
+            const Text(
+              "phamnhatquynh@gmail.com",
+              style: TextStyle(color: Color.fromARGB(255, 65, 65, 65)),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightGreen,
+                onPrimary: Colors.white,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0)),
+                minimumSize: Size(200, 50),
+              ),
+              child: const Text(
+                'Edit Profile',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {},
+            )
+          ],
         ),
       ),
     );
