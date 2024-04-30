79a018846da99882ccda87e1a779e989c0cb9ec3
Phạm Nhật Quỳnh <52100100@student.tdtu.edu.vn>
2024-05-01 04:22:13 +0700

    complete libra

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInPage(),
+      home: LibraryPage(),
     );
   }
 }
